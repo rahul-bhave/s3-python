ab97449f221aa546cc09c4a19494bb50461e8ef6
Rahul.Bhave <rahul.bhave@qxf2.com>
2019-12-04 15:24:32 +0530

    commited changes

--- tests/bucket_exists.py
+++ tests/bucket_exists.py
@@ -6,11 +6,9 @@ from botocore.exceptions import ClientError
 
 def bucket_exists(bucket_name):
     """Determine whether bucket_name exists and the user has permission to access it
-
     :param bucket_name: string
     :return: True if the referenced bucket_name exists, otherwise False
     """
-
     s3 = boto3.client('s3')
     try:
         response = s3.head_bucket(Bucket=bucket_name)
@@ -19,23 +17,19 @@ def bucket_exists(bucket_name):
         return False
     return True
 
-
 def main():
     """Exercise bucket_exists()"""
-
     # Assign this value before running the program
     test_bucket_name = 'rahulb-test-bucket'
-
     # Set up logging
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)s: %(asctime)s: %(message)s')
-
     # Check if the bucket exists
     if bucket_exists(test_bucket_name):
         logging.info(f'{test_bucket_name} exists and you have permission to access it.')
     else:
         logging.info(f'{test_bucket_name} does not exist or '
                      f'you do not have permission to access it.')
-
+                     
 if __name__ == '__main__':
     main()
